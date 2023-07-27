3162bd39a46b029d8b1bb57308d157b52f2b1a68
ashleytrollinger <ashley@trollingers.com>
2023-07-26 21:20:08 -0400

    work around for now with the comments because the prop is not returning anything but I want to move to something else for now

--- client/src/components/containers/CommentsContainer/CommentsContainer.js
+++ client/src/components/containers/CommentsContainer/CommentsContainer.js
@@ -1,21 +1,31 @@
 import Comment from '../../builders/Comment/Comment'
 
 
-const CommentsContainer = ({comments}) => {
-
+const CommentsContainer = ({ comments }) => {
+    if (!comments) {
+        return <h3>No comments have been left.</h3>;
+    }
     return (
         <>
             <section>
                 <div>
-                    {comments.map((comment) => {
-                        return <Comment authorName={comment.authorName} commentContent={comment.commentContent}/>
-                    })}
-
+                    {comments.length === 0 ? (
+                        <h3>No comments have been left.</h3>
+                    ) : (
+                        comments.map((comment) => (
+                            <Comment
+                                key={comment._id}
+                                authorName={comment.authorName}
+                                commentContent={comment.commentContent}
+                            />
+                        ))
+                    )}
                 </div>
-
             </section>
         </>
-    )
-}
+    );
+};
 
 export default CommentsContainer;
+
+
